e8b54fea0346f9c363c969948598734655304c65
JAIRO2025-wq <200248844+JAIRO2025-wq@users.noreply.github.com>
2026-05-21 14:12:09 -0600

    Add files via upload
    servicio de workers y manifest

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'valedigit-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // El evento fetch es obligatorio para que Chrome considere la app como instalable
+  event.respondWith(fetch(event.request).catch(() => {
+    return caches.match(event.request);
+  }));
+});
